2832066e7bb0e0dc5e783d60b0e94ffe418e63d1
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-09-10 14:18:30 -0500

    repartir cartas

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,54 +1,80 @@
-/**
- * 2C = Two of Clubs 
- * 2D = Two of Diaminds 
- * 2H = Two of Hearts
- * 2S = Two Spades  
- */
+let deck         = [];
+const tipos      = ['C','D','H', 'S'];
+const especiales = ['A','J','Q', 'K'];
 
- let deck = [];
- const tipos = ['C','D','H', 'S'];
- const especiales = ['A','J','Q', 'K'];
+let puntosJugador = 0,
+   puntosComputadora = 0;
+//Referencias de HTML
+const btnPedir = document.querySelector('#btnPedir');
 
-    //Esta funcion crea un nuevo Deck
- const crearDeck = () =>  {
+const divCartasJugador = document.querySelector('#jugador-cartas');
 
+const puntosHTML = document.querySelectorAll('small');
 
-    for(let i = 2; i<=10 ; i++){
-        for(let tipo of tipos){
-        deck.push(i + tipo);
-        }
-        
-    }
-
-    for(let tipo of tipos ){
-        for(let esp of especiales){
-        deck.push(esp + tipo);
-        }
-    }
-    //console.log(deck);
-    deck = _.shuffle( deck);
-    console.log(deck);
-    return deck;
+   //Esta funcion crea un nuevo Deck
+const crearDeck = () =>  {
 
 
-    }
+   for(let i = 2; i<=10 ; i++){
+       for(let tipo of tipos){
+       deck.push(i + tipo);
+       }
+       
+   }
 
-    crearDeck();
+   for(let tipo of tipos ){
+       for(let esp of especiales){
+       deck.push(esp + tipo);
+       }
+   }
+   //console.log(deck);
+   deck = _.shuffle( deck);
+   console.log(deck);
+   return deck;
 
-    //Esta funcion permite tomar una carta 
-    const pedirCarta = () => {
-        
-        if (deck.length === 0){
-            throw 'No hay cartas en el deck';
-        }
-        
-        const carta = deck.pop();
 
-        console.log(deck);
-        console.log(carta);
-        return'carta';
-    }
+   }
 
-        pedirCarta();
+   crearDeck();
+
+   //Esta funcion permite tomar una carta 
+   const pedirCarta = () => {
+       
+       if (deck.length === 0){
+           throw 'No hay cartas en el deck';
+       }
+       
+       const carta = deck.pop();
+       return carta;
+   }
+
+       //pedirCarta();
+       const valorCarta = (carta) =>{
+           
+           const valor = carta.substring(0, carta.length - 1);
+           return(isNaN(valor)) ?
+           (valor === 'A') ? 11 : 10
+               : valor * 1;
+          
+   }
+     
+   //Eventos
+   btnPedir.addEventListener('click', () => {
+       
+       const carta = pedirCarta();
+
+       puntosJugador =  puntosJugador + valorCarta(carta);
+       console.log(puntosJugador);
+       puntosHTML[0].innerText = puntosJugador;
+
+       //<img class="carta" src="../assets/cartas/3C.png" alt="">
+         const imgCarta = document.createElement('img');
+         imgCarta.src= `../assets/cartas/${carta}.png`;     
+         imgCarta.classList.add('carta');   
     
-    
+       divCartasJugador.append(imgCarta);
+
+   });
+
+
+
